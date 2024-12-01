18590b5df278592ddcc2935821207c2cb3064f94
kolsq <kolsq@KOLSQ>
2024-12-01 20:45:30 +0500

    Добавьте файлы проекта.

---
+++ WpfApp6/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp6
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Product> product = new ObservableCollection<Product>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Addproduct_Click(object sender, RoutedEventArgs e)
+        {
+            string name = NameText.Text;
+            int quantity = 0;
+
+            if (string.IsNullOrEmpty(NameText.Text))
+            {
+
+                MessageBox.Show("введите название");
+                return;
+
+            }
+
+            if (string.IsNullOrEmpty(PriceText.Text) || !float.TryParse(PriceText.Text, out float price))
+            {
+                MessageBox.Show("введите корректную цену");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(QuantityText.Text) || !int.TryParse(QuantityText.Text, out quantity))
+                {
+                 
+                        MessageBox.Show("введите корректное количество");
+                        return;
+            
+                }
+
+                var product = new Product(name, price, quantity);
+                ProductList.Items.Add(product);
+                Clear();
+
+
+            }
+
+        private void Clear()
+        {
+            NameText.Clear();
+            PriceText.Clear();
+            QuantityText.Clear();
+        }
+
+        private void Showproduct_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Product product in ProductList.Items) 
+            {
+                product.Deconstruct(out string name, out double price, out int quantity);
+                MessageBox.Show($"Название: {name}\nЦена: {price}\nКоличество: {quantity}");
+            }
+
+        }
+    }
+}
---
+++ WpfApp6/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp6
+{
+    public class Product
+    {
+        public Product(string name, float price, int quantity)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+        }
+
+        public Product(string name, float price)
+        {
+            Name = name;
+            Price = price;
+            Quantity = 0;
+        }
+
+        public void Deconstruct(out string name, out double price, out int quantity)
+        {
+            name = Name;
+            price = Price;
+            quantity = Quantity;
+        }
+
+        public string Name { get; set; }
+        public float Price { get; set; }
+        public int Quantity { get; set; }
+
+        public override string ToString()
+        {
+            return $"Название[{Name}]\nЦена[{Price} руб]\nКол[{Quantity} шт]";
+        }
+    }
+}
